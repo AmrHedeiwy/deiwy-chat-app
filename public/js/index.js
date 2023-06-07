1b4e26f46504c9b8e28341cfe5d6af4997b511bf
amr <amr.hedeiwy@gmail.com>
2023-06-07 17:39:55 +0100

    1- Settup frontend for register and login forms 2- Access to element values for both forms in client-side

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,35 @@
 const socket = io();
 
-socket.on("message", (message) => {
-  document.getElementById("1").innerHTML = message;
+const loginForm = document.querySelector('#loginForm');
+const registerForm = document.querySelector('#registerForm');
+
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const loginEmail = document.querySelector('#loginEmail').value;
+  const loginPssword = document.querySelector('#loginPassword').value;
+  const loginCheck = document.querySelector('#loginCheck').checked;
+
+  console.log(loginCheck, loginEmail, loginPssword);
+});
+
+registerForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const registerFirstName = document.querySelector('#registerFirstName').value;
+  const registerLastName = document.querySelector('#registerLastName').value;
+  const registerUsername = document.querySelector('#registerUsername').value;
+  const registerEmail = document.querySelector('#registerEmail').value;
+  const registerPassword = document.querySelector('#registerPassword').value;
+  const registerRepeatPassword = document.querySelector(
+    '#registerRepeatPassword'
+  ).value;
+  const registerCheck = document.querySelector('#registerCheck').checked;
+  console.log(
+    registerFirstName,
+    registerLastName,
+    registerUsername,
+    registerEmail,
+    registerPassword,
+    registerRepeatPassword,
+    registerCheck
+  );
 });
