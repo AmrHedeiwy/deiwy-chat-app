fb7758e1e14cc0c0fa320817513af5d03dea8467
amr <amr.hedeiwy@gmail.com>
2023-06-10 18:10:20 +0100

    User model validation added (database)

--- src/api/models/users.js
+++ src/api/models/users.js
@@ -1,6 +1,5 @@
-'use strict';
-const { v4: uuidv4 } = require('uuid');
 const { Model } = require('sequelize');
+const hooks = require('../services/hooks');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {}
@@ -16,14 +15,39 @@ module.exports = (sequelize, DataTypes) => {
       Firstname: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          is: {
+            args: /^[A-Za-z]{2,30}$/,
+            msg:
+              'First name can only contain letters,' +
+              'and must be between 2 and 30 characters long',
+          },
+        },
       },
       Lastname: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          is: {
+            args: /^[A-Za-z]{2,30}$/,
+            msg:
+              'Last name can only contain letters,' +
+              'and must be between 2 and 30 characters long',
+          },
+        },
       },
       Username: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          is: {
+            args: /^[A-Za-z\d_-]{3,20}$/,
+            msg:
+              'Username can only contain letters, digits, ' +
+              'underscores, and hyphens, and must be between ' +
+              '3 and 20 characters long.',
+          },
+        },
       },
       Userkey: {
         type: DataTypes.STRING,
@@ -33,24 +57,36 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         unique: true,
         allowNull: false,
+        validate: {
+          isEmail: {
+            msg:
+              'Please enter a valid email address' +
+              'in the format example@example.com.',
+          },
+        },
       },
       Password: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          is: {
+            args: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&*]{8,}$/,
+            msg:
+              'Password must be at least 8 characters long ' +
+              'and contain at least one uppercase letter, ' +
+              'one lowercase letter, one digit, ' +
+              'and one special character from the set @$!%?&.',
+          },
+        },
       },
     },
     {
       sequelize,
       modelName: 'User',
       tableName: 'users',
+      hooks,
     }
   );
-
-  User.beforeValidate((user) => {
-    const username = user.Username;
-    user.Userkey =
-      username + '#' + (parseInt(uuidv4().replace('-', ''), 16) % 10000);
-  });
 
   return User;
 };
