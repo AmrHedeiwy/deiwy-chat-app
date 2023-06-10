173f22b212dec267423676ea18079412ddbe5f26
amr <amr.hedeiwy@gmail.com>
2023-06-10 09:21:04 +0100

    User Model defiended

--- src/api/models/users.js
+++ src/api/models/users.js
@@ -1,20 +1,56 @@
+'use strict';
+const { v4: uuidv4 } = require('uuid');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'User',
+  class User extends Model {}
+
+  User.init(
     {
-      // Model attributes are defined here
-      firstName: {
+      UserID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      Firstname: {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      lastName: {
+      Lastname: {
         type: DataTypes.STRING,
-        // allowNull defaults to true
+        allowNull: false,
+      },
+      Username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      Userkey: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      Email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      Password: {
+        type: DataTypes.STRING,
+        allowNull: false,
       },
     },
     {
+      sequelize,
+      modelName: 'User',
       tableName: 'users',
     }
   );
+
+  User.beforeValidate((user) => {
+    const username = user.Username;
+    user.Userkey =
+      username + '#' + (parseInt(uuidv4().replace('-', ''), 16) % 10000);
+  });
+
   return User;
 };
